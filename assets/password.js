3c06c7a18e6bdbe2dadcbc4c572bf991aa27b387
McCabe Northup <mccabenorthup@gmail.com>
2020-02-09 21:33:31 -0700

    basic file structure and some styling

---
+++ assets/password.js
@@ -0,0 +1,32 @@
+let seconds = 10;
+const pauseVideo = () => {
+    setInterval(() => {
+        seconds--;
+        if (seconds === 0) {
+            stopFunc()
+            document.getElementById("myVideo").pause();
+            console.log("paused");
+            divToggle()
+        }
+    }, 1000);
+}
+pauseVideo()
+const divToggle = () => {
+    $("#passwordDiv").toggle("swing")
+}
+const stopFunc = () => {
+    clearInterval(pauseVideo())
+}
+
+$("#passSubmit").on("click", (e) => {
+    e.preventDefault();
+    console.log($("#passwordInput").val());
+    if($("#passwordInput").val() === "1995") {
+        location.replace("game.html")
+    } else {
+        location.replace("index.html")
+    }
+    
+    
+})
+
---
+++ assets/script.js
@@ -0,0 +1,3 @@
+console.log("%cThe Answer To This Question Is the Password You Seek!", "font-size: 15px;");
+console.log("*****What Year Was JavaScript Created?****");
+
